c4bb182f4b9d870ac9e22d6f74ab7ec09aca8455
Marc Tanti <marc.tanti@um.edu.mt>
2020-03-12 00:01:12 +0100

    Fix bug in time_space_complexity.py where label directories were not being passed correctly. Also focused on volume and area rather than length of volume side.

--- Python/tools/time_space_complexity.py
+++ Python/tools/time_space_complexity.py
@@ -13,6 +13,10 @@ from asemi_segmenter.lib import datas
 from asemi_segmenter.lib import times
 from asemi_segmenter import segmenter
 
+class Listener(segmenter.ProgressListener):
+    def error_output(self, text):
+        raise Exception(text)
+
 #########################################
 def measure(volume_cube_side_lengths, num_training_slices, num_labels, num_runs, train_config, max_processes, max_batch_memory, listener=lambda side_length, run, train_time, train_memory, segment_time, segment_memory:None):
     (_, featuriser, _) = datas.load_train_config_data(train_config)
@@ -56,7 +60,7 @@ def measure(volume_cube_side_lengths, num_training_slices, num_labels, num_runs,
                     datas.save_image(os.path.join(temp_dir, 'labels', '_{}'.format(label), '{}.tif'.format(i)), r.randint(0, 2, size=[side_length,side_length], dtype=np.uint16))
             
             print('Preprocessing volume')
-            segmenters.preprocess(
+            segmenter.preprocess(
                     volume_dir=os.path.join(temp_dir, 'volume'),
                     config=preprocess_config,
                     result_data_fullfname=os.path.join(temp_dir, 'full_volume.hdf'),
@@ -64,6 +68,7 @@ def measure(volume_cube_side_lengths, num_training_slices, num_labels, num_runs,
                     restart_checkpoint=False,
                     max_processes=max_processes,
                     max_batch_memory=max_batch_memory,
+                    listener=Listener()
                 )
             
             os.mkdir(os.path.join(temp_dir, 'segmentation'))
@@ -74,12 +79,12 @@ def measure(volume_cube_side_lengths, num_training_slices, num_labels, num_runs,
                 with times.Timer() as timer:
                     mem_usage = memory_profiler.memory_usage(
                         (
-                            segmenters.train,
+                            segmenter.train,
                             [],
                             dict(
                                 preproc_volume_fullfname=os.path.join(temp_dir, 'full_volume.hdf'),
                                 subvolume_dir=os.path.join(temp_dir, 'subvolume'),
-                                label_dirs=os.path.join(temp_dir, 'labels'),
+                                label_dirs=[ os.path.join(temp_dir, 'labels', '_{}'.format(label)) for label in range(num_labels) ],
                                 config=train_config,
                                 result_model_fullfname=os.path.join(temp_dir, 'model.pkl'),
                                 trainingset_file_fullfname=None,
@@ -87,16 +92,18 @@ def measure(volume_cube_side_lengths, num_training_slices, num_labels, num_runs,
                                 restart_checkpoint=False,
                                 max_processes=max_processes,
                                 max_batch_memory=max_batch_memory,
+                                listener=Listener()
                             )
                         )
                     )
+                    
                 train_time = timer.duration
                 train_memory = max(mem_usage)
                 
                 with times.Timer() as timer:
                     mem_usage = memory_profiler.memory_usage(
                         (
-                            segmenters.segment,
+                            segmenter.segment,
                             [],
                             dict(
                                 model=os.path.join(temp_dir, 'model.pkl'),
@@ -107,6 +114,7 @@ def measure(volume_cube_side_lengths, num_training_slices, num_labels, num_runs,
                                 soft_segmentation=True,
                                 max_processes=max_processes,
                                 max_batch_memory=max_batch_memory,
+                                listener=Listener()
                             )
                         )
                     )
@@ -123,77 +131,86 @@ def plot(volume_cube_side_lengths, num_training_slices, num_labels, num_runs, tr
 
     [ train_time_plot ] = axs[0,0].plot([], [], color='red', linestyle='', marker='o', markersize=2)
     axs[0,0].set_title('Train time')
-    axs[0,0].set_xlim(0, max(volume_cube_side_lengths))
-    axs[0,0].set_xlabel('cube side')
-    axs[0,0].set_ylim(0, 1000)
+    axs[0,0].set_xlim(0, 1)
+    axs[0,0].set_xlabel('slice area')
+    axs[0,0].set_ylim(0, 1)
     axs[0,0].set_ylabel('seconds')
     axs[0,0].grid(True)
     
     [ train_mem_plot ] = axs[0,1].plot([], [], color='red', linestyle='', marker='o', markersize=2)
     axs[0,1].set_title('Train memory')
-    axs[0,1].set_xlim(0, max(volume_cube_side_lengths))
-    axs[0,1].set_xlabel('cube side')
-    axs[0,1].set_ylim(0, 1000)
+    axs[0,1].set_xlim(0, 1)
+    axs[0,1].set_xlabel('slice area')
+    axs[0,1].set_ylim(0, 1)
     axs[0,1].set_ylabel('MB')
     axs[0,1].grid(True)
     
     [ seg_time_plot ] = axs[1,0].plot([], [], color='red', linestyle='', marker='o', markersize=2)
     axs[1,0].set_title('Segment time')
-    axs[1,0].set_xlim(0, max(volume_cube_side_lengths))
-    axs[1,0].set_xlabel('cube side')
-    axs[1,0].set_ylim(0, 1000)
+    axs[1,0].set_xlim(0, 1)
+    axs[1,0].set_xlabel('cube volume')
+    axs[1,0].set_ylim(0, 1)
     axs[1,0].set_ylabel('seconds')
     axs[1,0].grid(True)
     
     [ seg_mem_plot ] = axs[1,1].plot([], [], color='red', linestyle='', marker='o', markersize=2)
     axs[1,1].set_title('Segment memory')
-    axs[1,1].set_xlim(0, max(volume_cube_side_lengths))
-    axs[1,1].set_xlabel('cube side')
-    axs[1,1].set_ylim(0, 1000)
+    axs[1,1].set_xlim(0, 1)
+    axs[1,1].set_xlabel('cube volume')
+    axs[1,1].set_ylim(0, 1)
     axs[1,1].set_ylabel('MB')
     axs[1,1].grid(True)
 
     fig.tight_layout()
-    fig.show()
     
     with open('results.txt', 'w', encoding='utf-8') as f:
-        print('side_length', 'run', 'train_time', 'train_memory', 'segment_time', 'segment_memory', sep='\t', file=f)
-    all_cube_sides = []
+        print('side_length', 'slice_area', 'cube_volume', 'run', 'train_time', 'train_memory', 'segment_time', 'segment_memory', sep='\t', file=f)
+    all_slice_areas = []
+    all_cube_volumes = []
     all_train_times = []
     all_train_mems = []
     all_seg_times = []
     all_seg_mems = []
     
     def listener(side_length, run, train_time, train_memory, segment_time, segment_memory):
-        with open('results.txt', 'a', encoding='utf-8') as f:
-            print(side_length, run, train_time, train_memory, segment_time, segment_memory, sep='\t', file=f)
+        area = side_length**2
+        volume = side_length**3
         
-        all_cube_sides.append(side_length)
+        with open('results.txt', 'a', encoding='utf-8') as f:
+            print(side_length, area, volume, run, train_time, train_memory, segment_time, segment_memory, sep='\t', file=f)
+        
+        all_slice_areas.append(area)
+        all_cube_volumes.append(volume)
         all_train_times.append(train_time)
         all_train_mems.append(train_memory)
         all_seg_times.append(segment_time)
         all_seg_mems.append(segment_memory)
     
-        train_time_plot.set_data(all_cube_sides, all_train_times)
-        train_mem_plot.set_data(all_cube_sides, all_train_mems)
-        seg_time_plot.set_data(all_cube_sides, all_seg_times)
-        seg_mem_plot.set_data(all_cube_sides, all_seg_mems)
+        train_time_plot.set_data(all_slice_areas, all_train_times)
+        train_mem_plot.set_data(all_slice_areas, all_train_mems)
+        seg_time_plot.set_data(all_cube_volumes, all_seg_times)
+        seg_mem_plot.set_data(all_cube_volumes, all_seg_mems)
         
-        axs[0,0].set_ylim(0, max(all_train_times))
-        axs[0,1].set_ylim(0, max(all_train_mems))
-        axs[1,0].set_ylim(0, max(all_seg_times))
-        axs[1,1].set_ylim(0, max(all_seg_mems))
+        axs[0,0].set_ylim(0, max(all_train_times)*1.1)
+        axs[0,0].set_xlim(0, area*1.1)
+        axs[0,1].set_ylim(0, max(all_train_mems)*1.1)
+        axs[0,1].set_xlim(0, area*1.1)
+        axs[1,0].set_ylim(0, max(all_seg_times)*1.1)
+        axs[1,0].set_xlim(0, volume*1.1)
+        axs[1,1].set_ylim(0, max(all_seg_mems)*1.1)
+        axs[1,1].set_xlim(0, volume*1.1)
         
         fig.canvas.draw()
         fig.canvas.flush_events()
+        fig.savefig('results.png')
     
     measure(volume_cube_side_lengths, num_training_slices, num_labels, num_runs, train_config, max_processes, max_batch_memory, listener)
     
 plot(
-        volume_cube_side_lengths=list(range(100, 1000+1, 100)),
+        volume_cube_side_lengths=list(range(10, 200+1, 10)),
         num_training_slices=10,
         num_labels=5,
-        num_runs=5,
+        num_runs=3,
         train_config={
                 'featuriser': {
                         'type': 'histograms',
@@ -217,8 +234,8 @@ plot(
                         'sample_size_per_label': -1
                     }
             },
-        max_processes=2,
-        max_batch_memory=1
+        max_processes=1,
+        max_batch_memory=0.1
     )
 
 input('Press enter to exit.')
