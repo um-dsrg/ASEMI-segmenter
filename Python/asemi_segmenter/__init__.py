225902311b12dc7263a7986121159d43d71cb768
Marc Tanti <marc.tanti@um.edu.mt>
2020-04-15 16:36:07 +0200

    v0.1 Added version file, __version__, and versions in setup dependencies.

---
+++ Python/asemi_segmenter/__init__.py
@@ -0,0 +1,3 @@
+import pkg_resources
+
+__version__ = pkg_resources.resource_string('asemi_segmenter', 'version.txt').decode()
--- Python/setup.py
+++ Python/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='asemi_segmenter',
-        version='0.0.0',
+        version=open('asemi_segmenter/version.txt', 'r', encoding='utf-8').read(),
         packages=[
                 'asemi_segmenter',
                 'asemi_segmenter.lib',
@@ -10,22 +10,25 @@ setuptools.setup(
                 'asemi_segmenter.resources.json_schema'
             ],
         package_data={
-                'asemi_segmenter': ['resources/json_schema/*.json']
+                'asemi_segmenter': [
+                    'version.txt',
+                    'resources/json_schema/*.json'
+                    ]
             },
         install_requires=[
-                'fast-histogram',
-                'h5py',
-                'joblib',
-                'loky',
-                'numpy',
-                'Pillow',
-                'scikit-learn',
-                'scikit-image',
-                'scipy',
-                'tqdm',
-                'psutil',
-                'jsonschema',
-                'jsonref',
-                'memory_profiler'
+                'fast-histogram==0.7',
+                'h5py===2.10.0',
+                'joblib==0.14.0',
+                'jsonref==0.2',
+                'jsonschema==3.2.0',
+                'loky==2.6.0',
+                'memory_profiler==0.55.0',
+                'numpy==1.15.4',
+                'Pillow==6.2.1',
+                'psutil==5.6.7',
+                'scikit-image==0.14.1',
+                'scikit-learn==0.19.1',
+                'scipy==1.1.0',
+                'tqdm==4.43.0'
             ]
     )
