9b816db3fddede0fa83b64d735c7f843f9a4c0e5
Marc Tanti <marc.tanti@um.edu.mt>
2020-05-15 15:59:06 +0200

    setup.py now uses requirements.txt but strips away specific versions of dependencies.

--- Python/setup.py
+++ Python/setup.py
@@ -1,8 +1,13 @@
 import setuptools
 
+with open('asemi_segmenter/version.txt', 'r', encoding='utf-8') as f:
+    version = f.read().strip()
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = [line.split('==')[0] for line in f.read().strip().split('\n')]
+
 setuptools.setup(
         name='asemi_segmenter',
-        version=open('asemi_segmenter/version.txt', 'r', encoding='utf-8').read(),
+        version=version,
         packages=[
                 'asemi_segmenter',
                 'asemi_segmenter.lib',
@@ -12,24 +17,9 @@ setuptools.setup(
         package_data={
                 'asemi_segmenter': [
                     'version.txt',
-                    'resources/json_schema/*.json'
+                    'resources/json_schema/*.json',
+                    'resources/colours/*.json',
                     ]
             },
-        install_requires=[
-                'fast-histogram==0.7',
-                'h5py==2.10.0',
-                'joblib==0.14.0',
-                'jsonref==0.2',
-                'jsonschema==3.2.0',
-                'loky==2.6.0',
-                'matplotlib==2.1.2',
-                'memory_profiler==0.55.0',
-                'numpy==1.15.4',
-                'Pillow==6.2.1',
-                'psutil==5.6.7',
-                'scikit-image==0.14.1',
-                'scikit-learn==0.19.1',
-                'scipy==1.1.0',
-                'tqdm==4.43.0'
-            ]
+        install_requires=requirements
     )
