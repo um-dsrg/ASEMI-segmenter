b7d954bc6f6af7b2585f5f727535048b064fe344
Johann A. Briffa <johann.briffa@um.edu.mt>
2020-05-28 09:04:12 +0200

    Versioning using setuptools-scm

--- Python/setup.py
+++ Python/setup.py
@@ -1,13 +1,16 @@
 import setuptools
+from setuptools_scm import get_version
 
-with open('asemi_segmenter/version.txt', 'r', encoding='utf-8') as f:
-    version = f.read().strip()
+with open('asemi_segmenter/version.txt', 'w', encoding='utf-8') as f:
+    version_string = get_version(root="..", relative_to=__file__,
+            local_scheme="node-and-timestamp")
+    f.write(version_string)
 with open('requirements.txt', 'r', encoding='utf-8') as f:
     requirements = [line.split('==')[0] for line in f.read().strip().split('\n')]
 
 setuptools.setup(
         name='asemi_segmenter',
-        version=version,
+        version=version_string,
         packages=[
                 'asemi_segmenter',
                 'asemi_segmenter.lib',
