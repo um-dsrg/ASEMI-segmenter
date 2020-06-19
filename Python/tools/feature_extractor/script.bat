6d5f5ce559817a246c425cfe4d6378f659b5f91f
Johann A. Briffa <johann.briffa@um.edu.mt>
2020-06-19 16:15:47 +0200

    Separate examples for histogram and LBP features
    Also, in bash script, compute with and without GPU support

--- Python/tools/feature_extractor/script.bat
+++ Python/tools/feature_extractor/script.bat
@@ -1,9 +1,9 @@
 python feature_extractor.py ^
     --volume_fullfname "..\..\example_volume\output\preprocess\volume.hdf" ^
-    --feature_config_fullfname "feature_config.json" ^
+    --feature_config_fullfname "feature_config-histogram.json" ^
     --volume_slice_index 0 ^
     --volume_slice_count 10 ^
     --max_processes 1 ^
     --max_batch_memory 1 ^
-    --save_as "results.txt" ^
+    --save_as "results-histogram-gpu=no.txt" ^
     --use_gpu "no"
--- Python/tools/feature_extractor/script.sh
+++ Python/tools/feature_extractor/script.sh
@@ -1,9 +1,19 @@
-python feature_extractor.py \
-    --volume_fullfname "../../example_volume/output/preprocess/volume.hdf" \
-    --feature_config_fullfname "feature_config.json" \
-    --volume_slice_index 0 \
-    --volume_slice_count 10 \
-    --max_processes 1 \
-    --max_batch_memory 1 \
-    --save_as "results.txt" \
-    --use_gpu "no"
+#!/bin/bash
+
+for feature in histogram lbp; do
+   for gpu in no yes; do
+      output="results-$feature-gpu=$gpu.txt"
+      if [ ! -f "$output" ]; then
+         echo "Extracting features: $feature, gpu=$gpu..."
+         python feature_extractor.py \
+             --volume_fullfname "../../example_volume/output/preprocess/volume.hdf" \
+             --feature_config_fullfname "feature_config-$feature.json" \
+             --volume_slice_index 0 \
+             --volume_slice_count 10 \
+             --max_processes 1 \
+             --max_batch_memory 1 \
+             --save_as "$output" \
+             --use_gpu "$gpu"
+      fi
+   done
+done
