0819dc9243d22ff3edba7af303441f962bb9d341
Johann A. Briffa <johann.briffa@um.edu.mt>
2020-08-06 10:14:14 +0200

    Avoid regenerating already-computed output

--- Python/tools/masks_to_multi_roi/masks_to_multi_roi.py
+++ Python/tools/masks_to_multi_roi/masks_to_multi_roi.py
@@ -16,73 +16,80 @@ import multiprocessing as mp
 # image read/write functions
 
 def getchannels(im):
-   if ( im.mode.startswith('1') or
-      im.mode.startswith('L') or im.mode.startswith('P') or
-      im.mode.startswith('I') or im.mode.startswith('F') ):
-      return 1
-   if ( im.mode.startswith('RGBA') or im.mode.startswith('CMYK') ):
-      return 4
-   if ( im.mode.startswith('RGB') or im.mode.startswith('YCbCr') or
-      im.mode.startswith('LAB') or im.mode.startswith('HSV') ):
-      return 3
-   # this should never happen
-   return None
+    if ( im.mode.startswith('1') or
+        im.mode.startswith('L') or im.mode.startswith('P') or
+        im.mode.startswith('I') or im.mode.startswith('F') ):
+        return 1
+    if ( im.mode.startswith('RGBA') or im.mode.startswith('CMYK') ):
+        return 4
+    if ( im.mode.startswith('RGB') or im.mode.startswith('YCbCr') or
+        im.mode.startswith('LAB') or im.mode.startswith('HSV') ):
+        return 3
+    # this should never happen
+    return None
 
 def imwrite(I,outfile):
-   # Updated for PIL 1.1.6 upwards
-   # convert to image of the correct type based on shape and dtype
-   im = PIL.Image.fromarray(I.squeeze())
-   # save to file
-   im.save(outfile, compression="tiff_lzw", optimize=True)
-   return
+    # Updated for PIL 1.1.6 upwards
+    # convert to image of the correct type based on shape and dtype
+    im = PIL.Image.fromarray(I.squeeze())
+    # save to file
+    im.save(outfile, compression="tiff_lzw", optimize=True)
+    return
 
 def imread(infile):
-   im = PIL.Image.open(infile)
-   ch = getchannels(im)
-   (x,y) = im.size
-   # Updated for PIL 1.1.6 upwards
-   # return array is read-only!
-   I = np.asarray(im).reshape(y,x,ch)
-   return I
+    im = PIL.Image.open(infile)
+    ch = getchannels(im)
+    (x,y) = im.size
+    # Updated for PIL 1.1.6 upwards
+    # return array is read-only!
+    I = np.asarray(im).reshape(y,x,ch)
+    return I
 
 # method combining a set of individual-label images to a multi-ROI image
 
 def combine_labels(input_files, output_file, soft):
-   # read input images
-   stack = [imread(infile).squeeze() for infile in input_files]
-   # check for correct size and format
-   assert all(len(im.shape) == 2 for im in stack) # single channel
-   assert all(im.shape == stack[0].shape for im in stack) # same size
-   # insert null label
-   stack = [np.zeros(stack[0].shape, dtype=np.uint8)] + stack
-   # rearrange format and check internal consistency
-   stack = np.array(stack)
-   if soft:
-      stack = stack / 255.
-      if not (stack.sum(axis=0)<=1.).all():
-         print("Warning: probabilities sum to %f" % stack.sum(axis=0).max())
-   else:
-      assert np.logical_or(stack==0, stack==255).all() # binary masks
-      stack = stack // 255
-      assert (stack.sum(axis=0)<=1).all() # unique masks (not necessarily complete)
-   # convert to multi-ROI representation
-   I = stack.argmax(axis=0)
-   # write output image
-   imwrite(I.astype(np.uint8), output_file)
-   return
+    # read input images
+    stack = [imread(infile).squeeze() for infile in input_files]
+    # check for correct size and format
+    assert all(len(im.shape) == 2 for im in stack) # single channel
+    assert all(im.shape == stack[0].shape for im in stack) # same size
+    # insert null label
+    stack = [np.zeros(stack[0].shape, dtype=np.uint8)] + stack
+    # rearrange format and check internal consistency
+    stack = np.array(stack)
+    if soft:
+        stack = stack / 255.
+        if not (stack.sum(axis=0)<=1.).all():
+            print("Warning: probabilities sum to %f" % stack.sum(axis=0).max())
+    else:
+        assert np.logical_or(stack==0, stack==255).all() # binary masks
+        stack = stack // 255
+        assert (stack.sum(axis=0)<=1).all() # unique masks (not necessarily complete)
+    # convert to multi-ROI representation
+    I = stack.argmax(axis=0)
+    # write output image
+    imwrite(I.astype(np.uint8), output_file)
+    return
 
 # worker thread
 
-def worker(queue_in, queue_out, soft):
-   # iterate through slices to process
-   for i, input_files, output_file in iter(queue_in.get, None):
-      # do the work
-      combine_labels(input_files, output_file, soft)
-      # mark as done
-      queue_out.put(i)
-   # tell user we're done
-   print("Process %d exiting" % mp.current_process().pid)
-   return
+def worker(queue_in, queue_out, soft, force, dry_run):
+    # iterate through slices to process
+    for i, input_files, output_file in iter(queue_in.get, None):
+        # skip if output already exists
+        if os.path.isfile(output_file) and not force:
+            queue_out.put("skipped slice %i - output exists" % i)
+            continue
+        # skip if it's a dry run
+        if dry_run:
+            queue_out.put("would process slice %i" % i)
+            continue
+        # do the work
+        combine_labels(input_files, output_file, soft)
+        queue_out.put("processed slice %i" % i)
+    # tell user we're done
+    print("Process %d exiting" % mp.current_process().pid)
+    return
 
 ## main program
 
@@ -95,6 +102,10 @@ def main():
         help="output folder for multi-ROI image stack")
     parser.add_argument("-s", "--soft", action="store_true", default=False,
         help="input images contain probabilities, not binary masks")
+    parser.add_argument("-f", "--force", action="store_true", default=False,
+        help="regenerate output even if it already exists")
+    parser.add_argument("-n", "--dry-run", action="store_true", default=False,
+        help="show what will happen but do not create output")
     parser.add_argument("-p", "--processes", type=int, default=1,
         help="number of parallel processes")
     args = parser.parse_args()
@@ -109,9 +120,10 @@ def main():
         print("Warning: not all folders contain an equal number of files")
 
     # save labels
-    with open(os.path.join(args.output, 'labels.txt'), 'w', encoding='utf-8') as f:
-        for label in labels:
-            print(label, file=f)
+    if not args.dry_run:
+        with open(os.path.join(args.output, 'labels.txt'), 'w', encoding='utf-8') as f:
+            for label in labels:
+                print(label, file=f)
 
     # main timer
     start = time.time()
@@ -120,29 +132,24 @@ def main():
     queue_in = mp.Queue()
     queue_out = mp.Queue()
     for i in range(args.processes):
-        mp.Process(target=worker, args=(queue_in, queue_out, args.soft)).start()
+        mp.Process(target=worker, args=(queue_in, queue_out, args.soft, args.force, args.dry_run)).start()
 
     # iterate through each slice
     num_digits_in_filename = math.ceil(math.log10(N+1))
     for (i, images) in enumerate(zip(*filestack)):
-        print("Scheduling %d of %d..." % (i+1, N))
         paths = [os.path.join(a,b) for a,b in zip(args.input, images)]
-        queue_in.put(
-            (
-                i,
-                paths,
-                os.path.join(
+        output = os.path.join(
                     args.output,
                     "{:0>{}d}.tiff".format(i + 1, num_digits_in_filename)
                     )
-                )
-            )
+        print("Scheduling %d of %d..." % (i+1, N))
+        queue_in.put( (i, paths, output) )
 
     # wait on completion
     for i in range(N):
-        queue_out.get()
+        result = queue_out.get()
         ETA = str(timedelta(seconds=(time.time()-start)*(N-i-1)/(i+1)))
-        print("Completed %d of %d, ETA %s" % (i+1, N, ETA))
+        print("Completed %d of %d, ETA %s (%s)" % (i+1, N, ETA, result))
 
     # tell workers to stop
     for i in range(args.processes):
