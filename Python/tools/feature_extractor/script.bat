611277de731d1b399eb2b9895069f20329841925
Johann A. Briffa <johann.briffa@um.edu.mt>
2021-01-12 10:33:44 +0100

    Reverted removal of debug/timing results
    Compute separate results for:
    * histogram and LBP
    * GPU or CPU

--- Python/tools/feature_extractor/script.bat
+++ Python/tools/feature_extractor/script.bat
@@ -1,9 +1,16 @@
-python feature_extractor.py ^
-    --volume_fullfname "..\..\example_volume\output\preprocess\volume.hdf" ^
-    --feature_config_fullfname "feature_config.json" ^
-    --volume_slice_index 0 ^
-    --volume_slice_count 10 ^
-    --max_processes_featuriser 1 ^
-    --max_batch_memory 1 ^
-    --save_as "results.txt" ^
-    --use_gpu "no"
+FOR %%a IN (histogram lbp) DO (
+   FOR %%b in (no yes) DO (
+      SET o=results-%%a-gpu=%%b.txt
+      IF NOT EXIST %%o (
+         python feature_extractor.py ^
+             --volume_fullfname "..\..\example_volume\output\preprocess\volume.hdf" ^
+             --feature_config_fullfname "feature_config-%%a.json" ^
+             --volume_slice_index 0 ^
+             --volume_slice_count 10 ^
+             --max_processes_featuriser 1 ^
+             --max_batch_memory 1 ^
+             --save_as "%%o" ^
+             --use_gpu "%%b"
+      )
+   )
+)
--- Python/tools/feature_extractor/script.sh
+++ Python/tools/feature_extractor/script.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-python feature_extractor.py \
-    --volume_fullfname "../../example_volume/output/preprocess/volume.hdf" \
-    --feature_config_fullfname "feature_config.json" \
-    --volume_slice_index 0 \
-    --volume_slice_count 10 \
-    --max_processes_featuriser 1 \
-    --max_batch_memory 1 \
-    --save_as "results.txt" \
-    --use_gpu "no"
+for feature in histogram lbp; do
+   for gpu in no yes; do
+      output="results-$feature-gpu=$gpu.txt"
+      if [ ! -f "$output" ]; then
+         echo "Extracting features: $feature, gpu=$gpu..."
+         python feature_extractor.py \
+             --volume_fullfname "../../example_volume/output/preprocess/volume.hdf" \
+             --feature_config_fullfname "feature_config-$feature.json" \
+             --volume_slice_index 0 \
+             --volume_slice_count 10 \
+             --max_processes_featuriser 1 \
+             --max_batch_memory 1 \
+             --save_as "$output" \
+             --use_gpu "$gpu"
+      fi
+   done
+done
